fbfbab9c6b13993eac65fb490e7c65164776e10f
jmogno <JoaquinMognoUnahur>
2026-06-04 15:03:45 -0300

    correccion templates Mail

--- src/test/mail.test.js
+++ src/test/mail.test.js
@@ -1,4 +1,6 @@
 const { renderTemplate } = require('../modules/mail/mail.service');
+const fs = require('fs');
+const path = require('path');
 
 describe('Mail templates', () => {
   it('incluye la clave inicial en el correo de bienvenida', async () => {
@@ -9,5 +11,15 @@ describe('Mail templates', () => {
 
     expect(html).toContain('fue dada de alta con exito');
     expect(html).toContain('ClaveTemporal123');
+  });
+
+  it('no menciona UNAHUR en los templates de correo', () => {
+    const templatesDir = path.join(__dirname, '../modules/mail/templates');
+    const templateFiles = fs.readdirSync(templatesDir).filter((file) => file.endsWith('.html'));
+
+    for (const file of templateFiles) {
+      const html = fs.readFileSync(path.join(templatesDir, file), 'utf8');
+      expect(html).not.toMatch(/UNAHUR|MediUNAHUR/i);
+    }
   });
 });
